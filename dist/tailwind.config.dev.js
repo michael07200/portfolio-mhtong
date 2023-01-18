937ec46ab3d790c32e45cd1573b8e02642e15138
michael07200 <mickmhtong@gmail.com>
2023-01-18 21:14:32 +0100

    style: contact form

--- dist/tailwind.config.dev.js
+++ dist/tailwind.config.dev.js
@@ -1,8 +1,10 @@
+"use strict";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [require("@tailwindcss/forms"),],
+  plugins: [require("@tailwindcss/forms")]
 };
