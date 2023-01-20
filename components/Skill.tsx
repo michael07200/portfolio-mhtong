12bdaa8fe6cfac8060e36718454c3bc7423c6bd0
michael07200 <mickmhtong@gmail.com>
2023-01-20 17:56:26 +0100

    build: skills tagcloud

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,7 +1,63 @@
 import React from "react";
+import { Cloud, renderSimpleIcon } from "react-icon-cloud";
+import {
+  siJavascript,
+  siReact,
+  siTailwindcss,
+  siNodedotjs,
+  siExpress,
+  siMongodb,
+  siPostgresql,
+  siTypescript,
+  siNextdotjs,
+  siRedux,
+  siPython,
+  siGit,
+  siGithub,
+  siFigma,
+  siPostman,
+  siAdobephotoshop,
+  siAdobeillustrator,
+  siAdobeindesign,
+  siRhinoceros,
+} from "simple-icons";
 
 type Props = {};
 
 export default function Skill({}: Props) {
-  return <div>Skill</div>;
+  const icons = [
+    siJavascript,
+    siReact,
+    siTailwindcss,
+    siNodedotjs,
+    siExpress,
+    siMongodb,
+    siPostgresql,
+    siTypescript,
+    siNextdotjs,
+    siRedux,
+    siPython,
+    siGit,
+    siGithub,
+    siFigma,
+    siPostman,
+    siAdobephotoshop,
+    siAdobeillustrator,
+    siAdobeindesign,
+    siRhinoceros,
+  ].map((icon) => {
+    return renderSimpleIcon({
+      icon,
+      size: 60,
+      aProps: {
+        onClick: (e: any) => e.preventDefault(),
+      },
+    });
+  });
+
+  return (
+    <div>
+      <Cloud>{icons}</Cloud>
+    </div>
+  );
 }
