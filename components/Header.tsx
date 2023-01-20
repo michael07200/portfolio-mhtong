49ed0d32c5f8dac3d31d53d4cf8e863e9333a7bd
michael07200 <mickmhtong@gmail.com>
2023-01-20 07:45:05 +0100

    build: dark overlay for mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,17 +4,18 @@ import { motion } from "framer-motion";
 import Link from "next/link";
 import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from "react-icons/ai";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
-import { BsFillPersonLinesFill } from "react-icons/bs";
 import Image from "next/image";
 import NavLogo from "../public/assets/logo.svg";
 
 type Props = {};
+
 export default function Header({}: Props) {
+
   const [nav, setNav] = useState(false);
   const handleNav = () => {
     setNav(!nav);
   };
-
+ 
   return (
     <div>
       <header className="sticky top-0 flex justify-between max-w-7xl mx-auto z-20 xl:items-center">
@@ -86,82 +87,86 @@ export default function Header({}: Props) {
         </motion.div>
       </header>
 
-      {/* Side Drawer Menu */}
-      <div
-        className={
-          nav
-            ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#f7f4ed] p-10 ease-in duration-500"
-            : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
-        }
-      >
-        <div>
-          <div className="flex w-full items-center justify-between">
-            <Link href="/">
-              <Image src={NavLogo} width="50" height="50" alt="/" />
-            </Link>
-            <div
-              onClick={handleNav}
-              className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer"
-            >
-              <AiOutlineClose />
+      {/* Mobile Menu */}
+      {/* Overlay */}
+      <div className={nav ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70" : ""}>
+        {/* Side Drawer Menu */}
+        <div
+          className={
+            nav
+              ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#f7f4ed] p-10 ease-in duration-500"
+              : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
+          }
+        >
+          <div>
+            <div className="flex w-full items-center justify-between">
+              <Link href="/">
+                <Image src={NavLogo} width="50" height="50" alt="/" />
+              </Link>
+              <div
+                onClick={handleNav}
+                className="rounded-full shadow-lg shadow-gray-400 p-2 cursor-pointer"
+              >
+                <AiOutlineClose />
+              </div>
+            </div>
+            <div className="border-b border-gray-300 my-6">
+              <p className="w-[85%] md:w-[90%] pb-4">Michael Tong Portfolio</p>
             </div>
           </div>
-          <div className="border-b border-gray-300 my-4">
-            <p className="w-[85%] md:w-[90%] pb-4">Michael Tong Portfolio</p>
-          </div>
-        </div>
-        <div className="py-4 flex flex-col">
-          <ul className="uppercase">
-            <Link href="/">
-              <li onClick={() => setNav(false)} className="py-4 text-sm">
-                Home
-              </li>
-            </Link>
-            <Link href="/#about">
-              <li onClick={() => setNav(false)} className="py-4 text-sm">
-                About
-              </li>
-            </Link>
-            <Link href="/#experience">
-              <li onClick={() => setNav(false)} className="py-4 text-sm">
-                Experience
-              </li>
-            </Link>
-            <Link href="/#projects">
-              <li onClick={() => setNav(false)} className="py-4 text-sm">
-                Projects
-              </li>
-            </Link>
-            <Link href="/#contact">
-              <li onClick={() => setNav(false)} className="py-4 text-sm">
-                Contact
-              </li>
-            </Link>
-          </ul>
-          <div className="pt-40">
-            <div className="flex items-center justify-between my-4 w-[80%] sm:w-[50%]">
-              <a
-                href="https://www.linkedin.com/in/clint-briley-50056920a/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <div className="rounded-full shadow-lg shadow-gray-300 p-3 cursor-pointer hover:scale-110 ease-in duration-300">
-                  <FaLinkedinIn />
-                </div>
-              </a>
-              <a href="https://github.com/fireclint" target="_blank" rel="noreferrer">
-                <div className="rounded-full shadow-lg shadow-gray-300 p-3 cursor-pointer hover:scale-110 ease-in duration-300">
-                  <FaGithub />
-                </div>
-              </a>
-              <Link href="/#contact">
-                <div
-                  onClick={() => setNav(!nav)}
-                  className="rounded-full shadow-lg shadow-gray-300 p-3 cursor-pointer hover:scale-110 ease-in duration-300"
-                >
-                  <AiOutlineMail />
-                </div>
+          <div className="py-4 flex flex-col">
+            <ul className="uppercase">
+              <Link href="/">
+                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                  Home
+                </li>
               </Link>
+              <Link href="/#about">
+                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                  About
+                </li>
+              </Link>
+              <Link href="/#experience">
+                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                  Experience
+                </li>
+              </Link>
+              <Link href="/#projects">
+                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                  Projects
+                </li>
+              </Link>
+              <Link href="/#contact">
+                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                  Contact
+                </li>
+              </Link>
+            </ul>
+            <div className="pt-40">
+              <div className="flex items-center justify-between my-4 w-[80%] sm:w-[50%]">
+                <a
+                  href="https://www.linkedin.com/in/clint-briley-50056920a/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div className="rounded-full shadow-lg shadow-gray-300 p-3 cursor-pointer hover:scale-110 ease-in duration-300">
+                    <FaLinkedinIn />
+                  </div>
+                </a>
+                <a href="https://github.com/fireclint" target="_blank" rel="noreferrer">
+                  <div className="rounded-full shadow-lg shadow-gray-300 p-3 cursor-pointer hover:scale-110 ease-in duration-300">
+                    <FaGithub />
+                  </div>
+                </a>
+                <Link href="/#contact">
+                  <div
+                    onClick={() => setNav(!nav)}
+                    className="rounded-full shadow-lg shadow-gray-300 p-3 cursor-pointer hover:scale-110 ease-in duration-300"
+                  >
+                    <AiOutlineMail />
+                  </div>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
